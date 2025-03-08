934589ee1ffc87e7f037d276d6999226abd8ae7a
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-03-08 13:33:50 +1000

    checked_once

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,17 @@
 #Здесь прописываются все настройки Flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-#Создаем объект Flask
+
 app = Flask(__name__)
-
-#Устанавливаем секретный ключ
-app.config['SECRET_KEY'] = 'my_very_secret_key_try_to_guess_it'
-
-#Подключаем базу данных
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
-#Подключаем систему авторизации
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-#Подключаем шифрование паролей
-bcrypt = Bcrypt(app)
-
-#Подключаем маршруты
 from app import routes
